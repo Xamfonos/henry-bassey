ac7524e1a858f51dea5d99c11abb5712a2b50de7
xamfonos <henrymax90@gmail.com>
2026-03-03 16:00:31 -0800

    fix: explicit site URL for keystatic oauth redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 
 export default defineConfig({
+  site: 'https://henrybassey.com',
+
   output: 'static',
   adapter: vercel(),
   integrations: [tailwind(), react(), markdoc(), keystatic()],
