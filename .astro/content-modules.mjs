af9e3b1427d19b55fbf1cad52ab8b5b5ec1253fe
xamfonos <henrymax90@gmail.com>
2026-03-09 17:00:16 -0700

    fix: blog post formatting fixes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,4 @@
 
 export default new Map([
-["src/content/blog/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwelcome.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/seo-for-agentic-web.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fseo-for-agentic-web.mdx&astroContentModuleFlag=true")]]);
 		
